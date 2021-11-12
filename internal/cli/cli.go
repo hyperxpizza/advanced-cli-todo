fbd47c51ebfa6721d4c697a9cbfe4f0bbe804808
hyperxpizza <wojtek.frackowski@gmail.com>
2021-11-12 21:48:44 +0100

    done 4 today

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,11 +1,30 @@
 package cli
 
-import "github.com/hyperxpizza/advanced-cli-todo/internal/db"
+import (
+	"flag"
+
+	"github.com/hyperxpizza/advanced-cli-todo/internal/common"
+	"github.com/hyperxpizza/advanced-cli-todo/internal/config"
+	"github.com/hyperxpizza/advanced-cli-todo/internal/db"
+)
 
 type CLI struct {
 	db *db.Database
 }
 
-func Run() {
+func NewCLI(c *config.Config) (*CLI, error) {
+	logger := common.NewLogger(*loglevel)
+	database, err := db.NewDatabase(c, logger)
+	if err != nil {
+		return nil, err
+	}
 
+	defer database.Close()
+
+	return &CLI{db: database}, nil
+}
+
+func Run() {
+	flag.Parse()
+	printFlags()
 }
--- tests/flags.go
+++ tests/flags.go
@@ -5,4 +5,4 @@ import "flag"
 //flags required for running tests
 var configPathPtr = flag.String("config", "", "Path to file containing config.yml")
 var filePtr = flag.String("file", "", "Path to file")
-var loglevel = flag.String("loglevel", "debug", "level of logger|default:info")
+var loglevel = flag.String("loglevel", "info", "level of logger|default:info")
