3d4e315325b1af3bbfd7cc17a5a195eda3d8c611
hyperxpizza <wojtek.frackowski@gmail.com>
2021-11-12 18:56:30 +0100

    check if file exists function

--- internal/common/files.go
+++ internal/common/files.go
@@ -1,10 +1,18 @@
 package common
 
 import (
+	"errors"
 	"os"
+
+	"github.com/hyperxpizza/advanced-cli-todo/internal/customErrors"
 )
 
 //Checks if file with a provided path exists, if not, returns ErrFileNotFound
 func CheckIfFileExists(path string) error {
 	_, err := os.Stat(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return customErrors.Wrap(customErrors.ErrFileNotFound)
+	}
+
+	return nil
 }
