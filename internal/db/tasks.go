c1f9045cc47b5a46184854c601992a03fef6075c
hyperxpizza <wojtek.frackowski@gmail.com>
2021-11-13 08:47:06 +0100

    inserting a new task into the database

--- internal/db/tasks.go
+++ internal/db/tasks.go
@@ -1,9 +1,28 @@
 package db
 
-import "github.com/hyperxpizza/advanced-cli-todo/internal/models"
+import (
+	"time"
 
-func (db *Database) InsertTask() (int, error) {
-	var id int
+	"github.com/hyperxpizza/advanced-cli-todo/internal/models"
+)
+
+// Inserting a new task into the sqlite database
+func (db *Database) InsertTask(title, description string, priority int, due *time.Time) (int64, error) {
+	db.logger.Debugf("Inserting a new task with title: %s", title)
+	stmt, err := db.db.Prepare(`insert into tasks(id, title, description, priority, dueDate, created, updated) values(DEFAULT, $1, $2, $3, $4, $5, $6, $7)`)
+	if err != nil {
+		return 0, err
+	}
+
+	res, err := stmt.Exec(title, description, false, priority, due, time.Now(), time.Now())
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
 
 	return id, nil
 }
--- internal/models/task.go
+++ internal/models/task.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type Task struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Done        bool      `json:"done"`
-	Priority    int       `json:"priority"`
-	DueDate     time.Time `json:"dueDate"`
-	Created     time.Time `json:"created"`
-	Updated     time.Time `json:"updated"`
+	ID          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Done        bool       `json:"done"`
+	Priority    int        `json:"priority"`
+	DueDate     *time.Time `json:"dueDate"`
+	Created     time.Time  `json:"created"`
+	Updated     time.Time  `json:"updated"`
 }
 
 func NewTask(title, description string, done bool, priority int, due *time.Time) {
