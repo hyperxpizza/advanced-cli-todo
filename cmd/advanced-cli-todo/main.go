ef31b2ff133d63ad9fa12e26288d190bd070dfc3
hyperxpizza <wojtek.frackowski@gmail.com>
2021-11-13 09:27:25 +0100

    add flags and config to the main file

--- cmd/advanced-cli-todo/main.go
+++ cmd/advanced-cli-todo/main.go
@@ -2,20 +2,43 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+	"runtime"
 
 	"github.com/hyperxpizza/advanced-cli-todo/internal/common"
+	"github.com/hyperxpizza/advanced-cli-todo/internal/config"
+	"github.com/hyperxpizza/advanced-cli-todo/internal/runner"
 )
 
-var mode = flag.String("mode", "default", "mode of running the program. availiable options: web|cli|default")
+var mode = flag.String("mode", "default", "mode of running the program. availiable options: api|cli|default")
 var configPathPtr = flag.String("config", "", "Path to file containing config.yml")
 var loglevel = flag.String("loglevel", "info", "level of logger|default:info")
 
 func main() {
 	flag.Parse()
 
+	//init a new logger instance
 	logger := common.NewLogger(*loglevel)
 	logger.Infof("Running the app in mode: %s", *mode)
 
-	switch *mode
+	//load config from file
+	c, err := config.NewConfig(*configPathPtr)
+	if err != nil {
+		log.Fatalf("Could not load config from file: %s error: %s", *configPathPtr, err.Error())
+		os.Exit(1)
+	}
+
+	switch *mode {
+	case "default":
+		runner.RunInDefaultMode()
+	case "web":
+		runner.RunAPI()
+	case "cli":
+		runner.RunCli()
+	default:
+		logger.Errorf("Mode: %s unknown! Aborting...", *mode)
+		runtime.Goexit()
+	}
 
 }
