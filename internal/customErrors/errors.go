81df66a2db8b7c2097fe74f50d6e2cf3dde1c759
hyperxpizza <wojtek.frackowski@gmail.com>
2021-11-14 17:55:07 +0100

    add new error and remove tests.sh file

--- internal/customErrors/errors.go
+++ internal/customErrors/errors.go
@@ -6,6 +6,7 @@ const (
 	ErrFileNotFound         = "File was not found"
 	ErrTaskTitleNotValid    = "The task title is not valid"
 	ErrTaskPriorityNotValid = "The task priority is not valid"
+	ErrTaskIDNotFound       = "The task with provided id was not found"
 )
 
 //Wraps a predefiined message and returns it as a new error
--- tests.sh
+++
@@ -1,3 +0,0 @@
-#TestConnectToDB
-go test -v ./tests/database/ --run TestConnectToDB --config=/home/hyperxpizza/dev/golang/advanced-cli-todo/configs/config.yml --loglevel=debug
-
